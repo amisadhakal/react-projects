66d31090de0a80afb2801862c05ecfb1c9bc281b
amisadhakal <amisadhakal3@gmail.com>
2026-07-21 09:59:32 +0545

    update app.jsx

--- react-weather-application/src/App.jsx
+++ react-weather-application/src/App.jsx
@@ -3,7 +3,7 @@ const WeatherApp = () => {
 const[city,setCity] = useState("");
 const[weather,setWeather] = useState(null);
 const getWeather = async () => {
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=import.meta.env.VITE_WEATHER_API_KEY}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`;
 const response = await fetch(url);
 const data = await response.json();
 console.log(data);
@@ -15,8 +15,8 @@ return(
 <input value={city} onChange={(event)=>{
 setCity(event.target.value)}}/>
 <button onClick={getWeather}>Search</button>
-{
-weather&&(
+
+{weather && weather.main && (
 <>
 <h2>{weather.name}</h2>
 <h3>Temperature: {weather.main.temp}°C</h3>
